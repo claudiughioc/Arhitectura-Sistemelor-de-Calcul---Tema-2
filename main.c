0f700bedb28f63fe92adb08b8e8a687770ac255f
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-03-28 23:45:54 +0300

    Added dgbmv function in main and dgbmv in fortran
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

--- main.c
+++ main.c
@@ -5,14 +5,26 @@
 #define RANGE       100
 #define N           5
 #define M           4
+#define MIN(X,Y) ((X) < (Y) ?  (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ?  (X) : (Y))
 
-double general_A[5][4] = { {1, 1, 1, 0},
+double bA[5][4] = { {1, 1, 1, 0},
         {2, 2, 2, 2},
         {3, 3, 3, 3},
         {4, 4, 4, 4},
         {0, 5, 5, 5}};
+double A[8][4] = {{0, 0, 1, 2},
+        {0, 1, 2, 3},
+        {1, 2, 3, 4},
+        {2, 3, 4, 5},
+        {3, 4, 5, 0},
+        {4, 5, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}};
+double x[4] = {1, 2, 3, 4};
+double y[10] = {1, 0, 2, 0, 3, 0, 4, 0, 5, 0};
 
-
+/*
 double **generate_general_matrix(int n, int m)
 {
     int i, j;
@@ -34,7 +46,7 @@ double **generate_general_matrix(int n, int m)
 
 
     return NULL;
-}
+}*/
 
 void free_general_matrix(int n, int m, double **A)
 {
@@ -44,6 +56,7 @@ void free_general_matrix(int n, int m, double **A)
     free(A);
 }
 
+/*
 void display_matrix(int n, int m, double **a)
 {
     int i, j;
@@ -52,13 +65,104 @@ void display_matrix(int n, int m, double **a)
             printf("%lf ", general_A[i][j]);
         printf("\n");
     }
+}*/
+
+
+
+
+void dgbmv(int m, int n, int kl, int ku, double alpha, double **Ax, int lda,
+    double *xx, int incx, double beta, double *yy, int incy)
+{
+    int lenx, leny, kx, ky, iy, ix, kup1, i, jx, j, k;
+    double temp;
+    /* Sanity check */
+    if ((m <= 0) || (n <= 0) || (kl < 0) || (ku < 0) || (lda <= 0) ||
+        (lda < (kl + ku + 1)) || (incx = 0) || (incy == 0) ||
+        ((alpha == 0) && (beta == 1)))
+        return;
+    
+    lenx = n;
+    leny = m;
+
+    if (incx > 0)
+        kx = 0;
+    else
+        kx = - lenx * incx;
+
+    if (incy > 0)
+        ky = 0;
+    else
+        ky = - leny * incy;
+
+    if (beta != 1) {
+        if (incy == 1) {
+            if (beta == 0)
+                for (i = 0; i < leny; i++)
+                    y[i] = 0;
+            else
+                for (i = 0; i < leny; i++)
+                    y[i] = beta * y[i];
+        } else {
+            iy = ky;
+            if (beta == 0)
+                for (i = 0; i < leny; i++) {
+                    y[iy] = 0;
+                    iy += incy;
+                }
+            else
+                for (i = 0; i < leny; i++) {
+                    y[iy] = beta * y[iy];
+                    iy += incy;
+                }
+        }
+    }
+
+    if (alpha == 0)
+        return;
+
+    kup1 = ku + 1;
+    jx = kx;
+
+    if (incy == 1) {
+        for (j = 1; j < n; j++) {
+            if (x[jx] != 0) {
+                temp = alpha * x[jx];
+                k = kup1 - j;
+                for (i = MAX(1, j - ku); i < MIN(m, j + kl); i++)
+                    y[i] += temp * A[k + i][j];
+            }
+            jx += incx;
+        }
+    } else {
+        for (j = 1; j < n; j++) {
+            if (x[jx] != 0) {
+                temp = alpha * x[jx];
+                iy = ky;
+                k = kup1 - j;
+                for (i = MAX(1, j - ku); i < MIN(M, j + kl); i++) {
+                    y[iy] += temp * A[k + i][j];
+                    iy += incy;
+                }
+            }
+            jx += incx;
+            if (j > ku)
+                ky += incy;
+        }
+    }
+    return;
 }
 
 int main(void)
 {
     int n, m;
-    generate_general_matrix(5, 4);
-    display_matrix(5, 4, NULL);
+    //generate_general_matrix(5, 4);
+    //display_matrix(5, 4, NULL);
+
+    dgbmv(5, 4, 3, 2, 2, NULL, 8, NULL, 1, 10, NULL, 2);
+
+    int i = 0;
+    for (i = 0; i < 10; i++)
+        printf("%lf, ", y[i]);
 
     return 0;
 }
