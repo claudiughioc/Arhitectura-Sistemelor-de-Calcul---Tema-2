a849269e2b0560da18ab23b241c44b85177d15f6
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-03-31 14:02:35 +0300

    Use matrix A as a column oriented array
    *   finished my version of dgbmv
    
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

--- main.c
+++ main.c
@@ -3,42 +3,38 @@
 #include <time.h>
 
 #define RANGE       100
+#define X(I)        x[(I)-1]
+#define Y(I)        y[(I)-1]
+#define A(I, J)     bA[(I) - 1 + ((J) - 1) * lda]
 #define N           5
 #define M           4
 #define MIN(X,Y) ((X) < (Y) ?  (X) : (Y))
 #define MAX(X,Y) ((X) > (Y) ?  (X) : (Y))
 
-double bA[5][4] = { {1, 1, 1, 0},
+double nA[5][4] = { {1, 1, 1, 0},
         {2, 2, 2, 2},
         {3, 3, 3, 3},
         {4, 4, 4, 4},
         {0, 5, 5, 5}};
-double A[8][4] = {{0, 0, 1, 2},
-        {0, 1, 2, 3},
-        {1, 2, 3, 4},
-        {2, 3, 4, 5},
-        {3, 4, 5, 0},
-        {4, 5, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}};
+double bA[32] = {0, 0, 1, 2, 3, 4, 0, 0,
+                0, 1, 2, 3, 4, 5, 0, 0,
+                1, 2, 3, 4, 5, 0, 0, 0,
+                2, 3, 4, 5, 0, 0, 0, 0};
 double x[4] = {1, 2, 3, 4};
 double y[10] = {1, 0, 2, 0, 3, 0, 4, 0, 5, 0};
 
-/*
-double **get_random_values(int n, int m, double ** matrix)
+double **get_random_values(int n, int m, double **matrix)
 {
     int i, j;
     unsigned int seed = (unsigned int) time(NULL);
     srand(seed);
 
-
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
-            general_A[i][j] = ((double) rand() / (double) RAND_MAX) * RANGE;
+            matrix[i][j] = ((double) rand() / (double) RAND_MAX) * RANGE;
 
-
-    return NULL;
-}*/
+    return matrix;
+}
 
 void free_general_matrix(int n, int m, double **a)
 {
@@ -51,6 +47,7 @@ void free_general_matrix(int n, int m, double **a)
 void display_matrix(int n, int m, double **a)
 {
     int i, j;
+    printf("\nContinut matrice : \n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++)
             printf("%lf ", a[i][j]);
@@ -59,16 +56,14 @@ void display_matrix(int n, int m, double **a)
 }
 
 
-
-
-void dgbmv(int m, int n, int kl, int ku, double alpha, double **Ax, int lda,
+void dgbmv(int m, int n, int kl, int ku, double alpha, double *Ax, int lda,
     double *xx, int incx, double beta, double *yy, int incy)
 {
     int lenx, leny, kx, ky, iy, ix, kup1, i, jx, j, k;
     double temp;
     /* Sanity check */
     if ((m <= 0) || (n <= 0) || (kl < 0) || (ku < 0) || (lda <= 0) ||
-        (lda < (kl + ku + 1)) || (incx = 0) || (incy == 0) ||
+        (lda < (kl + ku + 1)) || (incx == 0) || (incy == 0) ||
         ((alpha == 0) && (beta == 1)))
         return;
     
@@ -76,33 +71,33 @@ void dgbmv(int m, int n, int kl, int ku, double alpha, double **Ax, int lda,
     leny = m;
 
     if (incx > 0)
-        kx = 0;
+        kx = 1;
     else
-        kx = - lenx * incx;
+        kx = 1 - (lenx - 1) * incx;
 
     if (incy > 0)
-        ky = 0;
+        ky = 1;
     else
-        ky = - leny * incy;
+        ky = 1 - (leny - 1) * incy;
 
     if (beta != 1) {
         if (incy == 1) {
             if (beta == 0)
-                for (i = 0; i < leny; i++)
-                    y[i] = 0;
+                for (i = 1; i <= leny; i++)
+                    Y(i) = 0;
             else
-                for (i = 0; i < leny; i++)
-                    y[i] = beta * y[i];
+                for (i = 1; i <= leny; i++)
+                    Y(i) = beta * Y(i);
         } else {
             iy = ky;
             if (beta == 0)
-                for (i = 0; i < leny; i++) {
-                    y[iy] = 0;
+                for (i = 1; i <= leny; i++) {
+                    Y(iy) = 0;
                     iy += incy;
                 }
             else
-                for (i = 0; i < leny; i++) {
-                    y[iy] = beta * y[iy];
+                for (i = 1; i <= leny; i++) {
+                    Y(iy) = beta * Y(iy);
                     iy += incy;
                 }
         }
@@ -115,23 +110,24 @@ void dgbmv(int m, int n, int kl, int ku, double alpha, double **Ax, int lda,
     jx = kx;
 
     if (incy == 1) {
-        for (j = 1; j < n; j++) {
-            if (x[jx] != 0) {
-                temp = alpha * x[jx];
+        for (j = 1; j <= n; ++j) {
+            if (X(jx) != 0) {
+                temp = alpha * X(jx);
                 k = kup1 - j;
-                for (i = MAX(1, j - ku); i < MIN(m, j + kl); i++)
-                    y[i] += temp * A[k + i][j];
+                for (i = MAX(1, j - ku); i <= MIN(m, j + kl); ++i) {
+                    Y(i) += temp * A(k + i, j);
+                }
             }
             jx += incx;
         }
     } else {
-        for (j = 1; j < n; j++) {
-            if (x[jx] != 0) {
-                temp = alpha * x[jx];
+        for (j = 1; j <= n; ++j) {
+            if (X(jx) != 0) {
+                temp = alpha * X(jx);
                 iy = ky;
                 k = kup1 - j;
-                for (i = MAX(1, j - ku); i < MIN(M, j + kl); i++) {
-                    y[iy] += temp * A[k + i][j];
+                for (i = MAX(1, j - ku); i <= MIN(m, j + kl); ++i) {
+                    Y(iy) += temp * A(k + i, j);
                     iy += incy;
                 }
             }
@@ -144,14 +140,42 @@ void dgbmv(int m, int n, int kl, int ku, double alpha, double **Ax, int lda,
 }
 
 
+double **to_band_storage(int n, int m, int lda, int kl, int ku, double **a)
+{
+    int u, v, i, j;
+    double **b = malloc(lda * sizeof(double *));
+    if (b == NULL)
+        return b;
+    for (i = 0; i < lda; i++) {
+        b[i] = malloc(m * sizeof(double));
+        if (b[i] == NULL) {
+            free(b);
+            return NULL;
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            if (j >= MAX(0, i - kl) && j < MIN(n, i + kl)) {
+                u = i + ku - j;
+                v = j;
+                if ( u >= 0 && u < lda)
+                    b[u][v] = a[i][j];
+            }
+        }
+    }
+
+    return b;
+}
 
 int main(void)
 {
-    int n = 5, m = 4;
-    //generate_general_matrix(5, 4);
-    //display_matrix(5, 4, NULL);
+    int n = 5, m = 4, i, lda = 8;
+    int kl = 4, ku = 3;
 
     double **a = malloc(n * sizeof(double *));
+    double **b;
+
     if (!a)
         perror("Error while allocating a\n");
     for (i = 0; i < n; i++) {
@@ -160,11 +184,21 @@ int main(void)
             perror("Error while allocating array in a\n");
     }
 
+    a = get_random_values(n, m, a);
+    display_matrix(n, m, a);
+
+    b = to_band_storage(n, m, lda, kl, ku, a);
+    display_matrix(lda, m, b);
+    
+
     dgbmv(5, 4, 3, 2, 2, NULL, 8, NULL, 1, 10, NULL, 2);
 
-    int i = 0;
+    /* Afisare vector final y */
+    printf("\nContinut vector y:\n");
     for (i = 0; i < 10; i++)
         printf("%lf, ", y[i]);
+    printf("\n");
 
+    free_general_matrix(n, m, a);
     return 0;
 }
